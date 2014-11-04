1f4a26d75f616de9e19c70f48e119ee42adb34aa
sarvdeepsingh092 <sarvdeep_singh@yahoo.in>
2014-11-03 20:01:28 -0500

    with comments push

--- rit_Lamp/src/rit_Lamp/Producer.java
+++ rit_Lamp/src/rit_Lamp/Producer.java
@@ -1,50 +1,84 @@
 package rit_Lamp;
 
+
+/**
+ * Producer.java
+ * 
+ * Version
+ * 			$id$
+ * 
+ * Revision
+ * 			$log$
+ */
 import java.util.*;
 
+
+/**
+ * This class is responsible for making the consumer Threads
+ * @author Sarvdeep Singh Bindra
+ * @author Sumedha Singh
+ *
+ */
 public class Producer {
 
 	public static void main(String args[]) {
+		//create two consumer objects
 		Consumer c1 = new Consumer();
 		Consumer c2 = new Consumer();
 
+		//create two more consumer objects and pass the previously created objects as arguments
 		Consumer c3 = new Consumer(c1);
 		Consumer c4 = new Consumer(c2);
 
+		//start the two threads
 		c3.start();
 		c4.start();
 
-		/*
-		 * try{ c3.join(); c4.join(); } catch(InterruptedException e){
-		 * 
-		 * }
-		 */
+		
+		 
 
 	}
 
 }
 
+/**
+ * 
+ * This class is responsible for creating threads for the various producers and creating the lamp
+ *
+ */
 class Consumer extends Thread {
+	
+	//declare a counter variable to run the run function that many times
 	static int counter = 10;
+	
+	//create reference of the Consumer
 	Consumer o;
+	
+	//create references of the producers
 	Screw screw;
 	LightBulb lb;
 	Socket socket;
 	Stand stand;
 	Base base;
 
-	/*
-	 * Screw screw1=new Screw(screw); LightBulb lb1=new LightBulb(lb); Socket
-	 * socket1=new Socket(socket); Stand stand1=new Stand(stand); Base base1=new
-	 * Base(base);
-	 */
+	
 
+	/**
+	 * default Constructor
+	 */
 	public Consumer() {
 
 	}
 
+	/**
+	 * parameterized constructor
+	 * @param o		The Consumer object passed
+	 */
 	public Consumer(Consumer o) {
+		//assign the passed object to the locally created reference
 		this.o = o;
+		
+		//create the objects of the producers and pass the consumer object to them
 		screw = new Screw(this.o);
 		lb = new LightBulb(this.o);
 		socket = new Socket(this.o);
@@ -52,15 +86,21 @@ class Consumer extends Thread {
 		base = new Base(this.o);
 	}
 
+	/**
+	 * This function creates the lamp and invokes the producers if the items needed are insufficient
+	 */
 	public void run() {
 		try {
 			while (counter != 0) {
 				synchronized (o) {
 
+					//check if the items are available
 					if (screw.screwList.size() >= 4 && base.baseList.size() >= 2 && socket.socketList.size() >= 7
 							&& lb.lightBulbList.size() >= 4 && stand.standList.size() >= 4) {
+						//if yes then lamp is created
 						System.out.println("Lamp created");
 
+						//remove those many items from the producers count
 						for (int i = 0; i < 4; i++) {
 							screw.screwList.remove(i);
 						}
@@ -77,6 +117,8 @@ class Consumer extends Thread {
 						for (int i = 0; i < 7; i++) {
 							socket.socketList.remove(i);
 						}
+						
+						//notify the consumer to wake up
 						o.notify();
 
 					} else {
@@ -84,11 +126,14 @@ class Consumer extends Thread {
 
 							System.out.println("starting production");
 
+							//invoke the producers
 							screw.start();
 							base.start();
 							socket.start();
 							stand.start();
 							lb.start();
+							
+							//make the consumer wait
 							o.wait();
 						} catch (Exception e) {
 
@@ -104,28 +149,41 @@ class Consumer extends Thread {
 
 }
 
+/**
+ * 
+ * This class is responsible for creating the screws needed
+ *
+ */
 class Screw extends Thread {
 
+	//initialize counter to run the run method that many times
 	static int counterS=10;
+	
+	//declare and initialize an arraylist
 	ArrayList<Integer> screwList = new ArrayList<Integer>();
 
+	//declare reference of the consumer
 	Consumer o;
 
-	// Object s;
+	/**
+	 * parameterized constructor assigns passed object to the consumer reference
+	 * @param o		passed object
+	 */
 	public Screw(Consumer o) {
 		this.o = o;
 	}
 
-	/*
-	 * public Screw(Object s){ this.s=s;
-	 * 
-	 * }
-	 */
+	
 
+	/**
+	 * This method creates 4 screws at a time and waits if the count is full
+	 */
 	public void run() {
 
 		while (counterS!=0) {
 			synchronized (o) {
+				
+				//check conditions and generate screws
 				if (screwList.size() <= 20 || screwList.size() % 4 == 0) {
 					for (int i = 0; i < 4; i++) {
 						screwList.add(1);
@@ -134,6 +192,7 @@ class Screw extends Thread {
 
 				} else {
 					try {
+						
 						o.wait();
 					} catch (Exception e) {
 
@@ -146,28 +205,44 @@ class Screw extends Thread {
 	}
 }
 
+/**
+ * 
+ * This class makes the base needed for the lamp
+ * it makes 2 bases at a time and waits if the count is full
+ *
+ */
 class Base extends Thread {
 
+	//declare the counter to run the run method
 	static int counterB=10;
+	
+	//declare the arraylist
 	ArrayList<Integer> baseList = new ArrayList<Integer>(10);
 
+	//declare the consumer reference
 	Consumer o;
 
-	// Object b;
+	
 
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public Base(Consumer o) {
 		this.o = o;
 	}
 
-	/*
-	 * public Base(Object b){ this.b=b; }
-	 */
+	
 
+	/**
+	 * This method creates 2 bases at a time
+	 */
 	public void run() {
 
 		while (counterB!=0) {
 			synchronized (o) {
 
+				//check condition and create base
 				if (baseList.size() < 10 || baseList.size() % 2 == 0) {
 
 					for (int i = 0; i < 2; i++) {
@@ -188,28 +263,43 @@ class Base extends Thread {
 	}
 }
 
+/**
+ * 
+ * This class makes the lightbulbs needed for the lamp
+ *
+ */
 class LightBulb extends Thread {
 
+	//declare the counter to run the run method
 	static int counterL=10;
+	
+	//declare the arraylist
 	ArrayList<Integer> lightBulbList = new ArrayList<Integer>(20);
 
+	//declare the consumer reference
 	Consumer o;
 
-	// Object l;
+	
 
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public LightBulb(Consumer o) {
 		this.o = o;
 	}
 
-	/*
-	 * public LightBulb(Object l){ this.l=l; }
-	 */
+	
 
+	/**
+	 * This method creates the lightbulbs needed for the lamp
+	 */
 	public void run() {
 
 		while (counterL!=0) {
 			synchronized (o) {
 
+				//check condition and create the lightbulbs  
 				if (lightBulbList.size() < 20 || lightBulbList.size() % 4 == 0) {
 
 					for (int i = 0; i < 4; i++) {
@@ -231,28 +321,41 @@ class LightBulb extends Thread {
 	}
 }
 
+/**
+ * 
+ * This class creates the stand for the lamp
+ *
+ */
 class Stand extends Thread {
 
+	//declare the counter to run the run method
 	static int counterST=10;
+	
+	//declare the arraylist
 	ArrayList<Integer> standList = new ArrayList<Integer>(20);
 
+	//declare the consumer reference
 	Consumer o;
 
-	// Object S;
-
+	
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public Stand(Consumer o) {
 		this.o = o;
 	}
 
-	/*
-	 * public Stand(Object S){ this.S=S; }
+	/**
+	 * this method creates 4 stands at a time for the lamp
 	 */
-
 	public void run() {
 
 		while (counterST!=0) {
 
 			synchronized (o) {
+				
+				//check condition and make stands
 				if (standList.size() < 20 || standList.size() % 4 == 0) {
 
 					for (int i = 0; i < 4; i++) {
@@ -272,24 +375,43 @@ class Stand extends Thread {
 	}
 }
 
+/**
+ * 
+ * This class makes the sockets for the lamps
+ *
+ */
 class Socket extends Thread {
 
+	//declare the counter to run the run method
 	static int counterSO=10;
+	
+	//declare the arraylist
 	ArrayList<Integer> socketList = new ArrayList<Integer>(35);
 
 	Object so;
 
+	//declare the consumer reference
 	Consumer o;
 
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public Socket(Consumer o) {
+		
 		this.o = o;
 	}
 
+	/**
+	 * This method creates 7 sockets for the lamp at a time
+	 */
 	public void run() {
 
 		while (counterSO!=0) {
 
 			synchronized (o) {
+				
+				//check the conditions and make the sockets
 				if (socketList.size() < 35 || socketList.size() % 7 == 0) {
 
 					for (int i = 0; i < 7; i++) {
--- rit_Lamp/src/rit_Lamp/Producer1.java
+++ rit_Lamp/src/rit_Lamp/Producer1.java
@@ -1,50 +1,86 @@
 package rit_Lamp;
 
+
+/**
+ * Producer.java
+ * 
+ * Version
+ * 			$id$
+ * 
+ * Revision
+ * 			$log$
+ */
+
 import java.util.*;
+
+/**
+ * This class is responsible for making the consumer Threads
+ * @author Sarvdeep Singh Bindra
+ * @author Sumedha Singh
+ *
+ */
 
 public class Producer1 {
 
 	public static void main(String args[]) {
+		//create two consumer objects
 		Consumer1 c1 = new Consumer1();
 		Consumer1 c2 = new Consumer1();
 
+		//create two more consumer objects and pass the previously created objects as arguments
 		Consumer1 c3 = new Consumer1(c1);
 		Consumer1 c4 = new Consumer1(c2);
 
+		//start the two threads
 		c3.start();
 		c4.start();
 
-		/*
-		 * try{ c3.join(); c4.join(); } catch(InterruptedException e){
-		 * 
-		 * }
-		 */
 
 	}
 
 }
 
+/**
+ * 
+ * This class is responsible for creating threads for the various producers and creating the lamp
+ *
+ */
+
 class Consumer1 extends Thread {
+	
+	//declare a counter variable to run the run function that many times
 	static int counter = 10;
+	
+	//create reference of the Consumer
 	Consumer1 o;
+	
+	//create references of the producers
 	Screw1 screw;
 	LightBulb1 lb;
 	Socket1 socket;
 	Stand1 stand;
 	Base1 base;
 
-	/*
-	 * Screw screw1=new Screw(screw); LightBulb lb1=new LightBulb(lb); Socket
-	 * socket1=new Socket(socket); Stand stand1=new Stand(stand); Base base1=new
-	 * Base(base);
-	 */
+	
 
+	/**
+	 * default Constructor
+	 */
 	public Consumer1() {
 
 	}
 
+	/**
+	 * parameterized constructor
+	 * @param o		The Consumer object passed
+	 */
+
 	public Consumer1(Consumer1 o) {
+		
+		//assign the passed object to the locally created reference
 		this.o = o;
+		
+		//create the objects of the producers and pass the consumer object to them
 		screw = new Screw1(this.o);
 		lb = new LightBulb1(this.o);
 		socket = new Socket1(this.o);
@@ -52,15 +88,24 @@ class Consumer1 extends Thread {
 		base = new Base1(this.o);
 	}
 
+	
+
+	/**
+	 * This function creates the lamp and invokes the producers if the items needed are insufficient
+	 */
 	public void run() {
 		try {
 			while (counter != 0) {
 				synchronized (o) {
 
+					//check if the items are available
 					if (Screw1.countScrew >= 4 && Base1.countBase >= 2 && Socket1.countSocket >= 7
 							&& LightBulb1.countLB >= 4 && Stand1.countStand >= 4) {
+						
+						//if yes then lamp is created
 						System.out.println("Lamp created");
 
+						//remove those many items from the producers count
 						for (int i = 0; i < 4; i++) {
 							Screw1.countScrew--;
 						}
@@ -77,6 +122,8 @@ class Consumer1 extends Thread {
 						for (int i = 0; i < 7; i++) {
 							Socket1.countSocket--;
 						}
+						
+						//notify the consumer to wake up
 						o.notify();
 
 					} else {
@@ -84,11 +131,14 @@ class Consumer1 extends Thread {
 
 							System.out.println("starting production");
 
+							//invoke the producers
 							screw.start();
 							base.start();
 							socket.start();
 							stand.start();
 							lb.start();
+							
+							//make the consumer wait
 							o.wait();
 						} catch (Exception e) {
 
@@ -104,30 +154,42 @@ class Consumer1 extends Thread {
 
 }
 
+/**
+ * 
+ * This class is responsible for creating the screws needed
+ *
+ */
 class Screw1 extends Thread {
 
+	//initialize counter to run the run method that many times
 	static int counterS = 10;
 
+	//declare and initialize counter
 	static int countScrew = 20;
-	// ArrayList<Integer> screwList = new ArrayList<Integer>();
+	
 
+	//declare reference of the consumer
 	Consumer1 o;
 
-	// Object s;
+	/**
+	 * parameterized constructor assigns passed object to the consumer reference
+	 * @param o		passed object
+	 */
 	public Screw1(Consumer1 o) {
 		this.o = o;
 	}
 
-	/*
-	 * public Screw(Object s){ this.s=s;
-	 * 
-	 * }
-	 */
+	
 
+	/**
+	 * This method creates 4 screws at a time and waits if the count is full
+	 */
 	public void run() {
 
 		while (counterS != 0) {
 			synchronized (o) {
+				
+				//check conditions and generate screws
 				if (countScrew <= 20 || countScrew % 4 == 0) {
 					for(int i=0;i<4;i++){
 						countScrew++;
@@ -148,30 +210,44 @@ class Screw1 extends Thread {
 	}
 }
 
+
+/**
+ * 
+ * This class makes the base needed for the lamp
+ * it makes 2 bases at a time and waits if the count is full
+ *
+ */
 class Base1 extends Thread {
 
+	//declare the counter to run the run method
 	static int counterB = 10;
 
+	//declare and initialize the counter
 	static int countBase = 10;
-	// ArrayList<Integer> baseList = new ArrayList<Integer>(10);
-
+	//declare the consumer reference
 	Consumer1 o;
 
-	// Object b;
+	
 
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public Base1(Consumer1 o) {
 		this.o = o;
 	}
 
-	/*
-	 * public Base(Object b){ this.b=b; }
-	 */
+	
 
+	/**
+	 * This method creates 2 bases at a time
+	 */
 	public void run() {
 
 		while (counterB != 0) {
 			synchronized (o) {
 
+				//check condition and create base
 				if (countBase < 10 || countBase % 2 == 0) {
 					for(int i=0;i<2;i++){
 						countBase++;
@@ -192,30 +268,43 @@ class Base1 extends Thread {
 	}
 }
 
+/**
+ * 
+ * This class makes the lightbulbs needed for the lamp
+ *
+ */
 class LightBulb1 extends Thread {
 
+	//declare the counter to run the run method
 	static int counterL = 10;
 
+	//declare and initialize the counter
 	static int countLB = 20;
-	// ArrayList<Integer> lightBulbList = new ArrayList<Integer>(20);
+	
 
+	//declare the consumer reference
 	Consumer1 o;
 
-	// Object l;
+	
 
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public LightBulb1(Consumer1 o) {
 		this.o = o;
 	}
 
-	/*
-	 * public LightBulb(Object l){ this.l=l; }
-	 */
+	
 
+	/**
+	 * This method creates the lightbulbs needed for the lamp
+	 */
 	public void run() {
 
 		while (counterL != 0) {
 			synchronized (o) {
-
+				//check condition and create the lightbulbs  
 				if (countLB < 20 || countLB % 4 == 0) {
 					for(int i=0;i<4;i++){
 					countLB++;
@@ -236,30 +325,44 @@ class LightBulb1 extends Thread {
 	}
 }
 
+/**
+ * 
+ * This class creates the stand for the lamp
+ *
+ */
 class Stand1 extends Thread {
 
+	//declare the counter to run the run method
 	static int counterST = 10;
 
+	//declare and initialize the counter
 	static int countStand = 20;
-	// ArrayList<Integer> standList = new ArrayList<Integer>(20);
-
+	
+	//declare the consumer reference
 	Consumer1 o;
 
-	// Object S;
+	
 
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public Stand1(Consumer1 o) {
 		this.o = o;
 	}
 
-	/*
-	 * public Stand(Object S){ this.S=S; }
-	 */
+	
 
+	/**
+	 * this method creates 4 stands at a time for the lamp
+	 */
 	public void run() {
 
 		while (counterST != 0) {
 
 			synchronized (o) {
+		
+				//check condition and make stands
 				if (countStand < 20 || countStand % 4 == 0) {
 					for(int i=0;i<4;i++){
 					countStand++;
@@ -278,26 +381,43 @@ class Stand1 extends Thread {
 	}
 }
 
+
+/**
+ * 
+ * This class makes the sockets for the lamps
+ *
+ */
 class Socket1 extends Thread {
 
+	//declare the counter to run the run method
 	static int counterSO = 10;
 
+	//declare and initialize the counter
 	static int countSocket = 35;
-	// ArrayList<Integer> socketList = new ArrayList<Integer>(35);
-
+	
 	Object so;
 
+	//declare the consumer reference
 	Consumer1 o;
 
+	/**
+	 * parameterized constructor assigns value of passed object to object reference created
+	 * @param o		passed objects
+	 */
 	public Socket1(Consumer1 o) {
 		this.o = o;
 	}
 
+	/**
+	 * This method creates 7 sockets for the lamp at a time
+	 */
 	public void run() {
 
 		while (counterSO != 0) {
 
 			synchronized (o) {
+		
+				//check the conditions and make the sockets
 				if (countSocket < 35 || countSocket % 7 == 0) {
 					for(int i=0;i<7;i++){
 					countSocket++;
